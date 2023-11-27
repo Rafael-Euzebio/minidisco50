08b431e7e10070ba93f6c86f85309c7358853a40
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2023-11-27 00:03:02 -0300

    refactor: replace high-level api with low-level api
    the high-level api was replaced because the low-level api is more flexible

--- src/play_song.c
+++ src/play_song.c
@@ -3,22 +3,55 @@
 #define MINIAUDIO_IMPLEMENTATION
 #include "miniaudio/include/miniaudio.h"
 
+void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
+{
+    ma_decoder* pDecoder = (ma_decoder*)pDevice->pUserData;
+    if (pDecoder == NULL) {
+        return;
+    }
+
+    ma_decoder_read_pcm_frames(pDecoder, pOutput, frameCount, NULL);
+
+    (void)pInput;
+}
+
 int play_song(char *path)
 {
     ma_result result;
-    ma_engine engine;
+    ma_decoder decoder;
+    ma_device_config deviceConfig;
+    ma_device device;
 
-    result = ma_engine_init(NULL, &engine);
+    result = ma_decoder_init_file(path, NULL, &decoder);
     if (result != MA_SUCCESS) {
-        return -1;
+        return -2;
     }
 
-    ma_engine_play_sound(&engine, path, NULL);
+    deviceConfig = ma_device_config_init(ma_device_type_playback);
+    deviceConfig.playback.format   = decoder.outputFormat;
+    deviceConfig.playback.channels = decoder.outputChannels;
+    deviceConfig.sampleRate        = decoder.outputSampleRate;
+    deviceConfig.dataCallback      = data_callback;
+    deviceConfig.pUserData         = &decoder;
+
+    if (ma_device_init(NULL, &deviceConfig, &device) != MA_SUCCESS) {
+        printf("Failed to open playback device.\n");
+        ma_decoder_uninit(&decoder);
+        return -3;
+    }
+
+    if (ma_device_start(&device) != MA_SUCCESS) {
+        printf("Failed to start playback device.\n");
+        ma_device_uninit(&device);
+        ma_decoder_uninit(&decoder);
+        return -4;
+    }
 
     printf("Press Enter to quit...");
     getchar();
 
-    ma_engine_uninit(&engine);
+    ma_device_uninit(&device);
+    ma_decoder_uninit(&decoder);
 
     return 0;
 }
